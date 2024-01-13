11bb265ed2b371f18d25d9fd52add638b873d808
jeff skoldberg <jeff@gmdata.co>
2024-01-13 14:53:28 -0500

    Removed test models and dependencies

--- models/package_development/raw.sql
+++
@@ -1,2 +0,0 @@
-select * from {{ source('google_sheets', 'sample_data') }}
-order by id
--- models/package_development/transformed.sql
+++
@@ -1,2 +0,0 @@
-
-{{ portable_google_sheets( 'google_sheets', 'sample_data',include_metadata=True ,keep_sort=False ) }}
